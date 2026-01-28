04304543644064ce68b11d87f92326fe3a65af60
z9306466-dev <z9306466@gmail.com>
2026-01-28 23:32:37 +0200

    Update audio_analysis.py

--- audio_analysis.py
+++ audio_analysis.py
@@ -1,11 +1,21 @@
 import librosa
 import numpy as np
+import random
 
 def analyze_audio(path):
     y, sr = librosa.load(path, sr=None)
     mfcc = librosa.feature.mfcc(y=y, sr=sr)
     score = np.var(mfcc)
-
+    
+    # العشوائية في النتيجة
+    random_variation = random.randint(-10, 10)
+    score += random_variation
+    
     if score < 60:
-        return "AI Generated", 85
-    return "Real", 78
+        result = "AI Generated"
+        confidence = 85 + random.randint(-5, 5)  # عشوائية في النسبة
+    else:
+        result = "Real"
+        confidence = 100 + random.randint(-5,5 )  # عشوائية في النسبة
+
+    return result, confidence
